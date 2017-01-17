31e8e3e07e37b1e9122852aae8b1a6ca05989cd4
Robb Eads <robb@eadsfamily.us>
2017-01-17 11:46:53 -0500

    Implemented Redis cache provider

---
+++ src/MindSung.Caching.Providers.Redis/RedisProvider.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using StackExchange.Redis;
+using System.Threading;
+
+namespace MindSung.Caching.Providers.Redis
+{
+    public class RedisProvider : ICacheProvider<string>, IDisposable
+    {
+        // Must be instantiated by the factory.
+        internal RedisProvider(ConnectionMultiplexer connection, string keyPrepend, bool slidingExpiry)
+        {
+            if (connection == null)
+            {
+                throw new ArgumentException("Redis cache connection cannot be null.", nameof(connection));
+            }
+            if (string.IsNullOrWhiteSpace(keyPrepend))
+            {
+                throw new ArgumentException("Redis cache provider requires a key-prepender value, it does not support multiple named caches.", nameof(keyPrepend));
+            }
+
+            this.connection = connection;
+            db = connection.GetDatabase();
+            this.keyPrepend = keyPrepend + ".";
+            this.slidingExpiry = slidingExpiry;
+            keySetChannel = this.keyPrepend + "provider_keyset";
+            keyDelChannel = this.keyPrepend + "provider_keydel";
+
+            keySetHandler = (_, keySet) => subHelper.PublishSet(keySet);
+            keyDelHandler = (_, keyDel) => subHelper.PublishDelete(keyDel);
+        }
+
+        private ConnectionMultiplexer connection;
+        private string keyPrepend;
+        private bool slidingExpiry;
+        private IDatabase db;
+        private ISubscriber sub;
+        private SemaphoreSlim subSync = new SemaphoreSlim(1, 1);
+        private CacheSubscriptionHelper subHelper = new CacheSubscriptionHelper();
+        private string keySetChannel;
+        private string keyDelChannel;
+        private Action<RedisChannel, RedisValue> keySetHandler;
+        private Action<RedisChannel, RedisValue> keyDelHandler;
+
+        private string FullKey(string key)
+        {
+            return keyPrepend + key;
+        }
+
+        private string ExpiryKey(string key)
+        {
+            return FullKey(key) + ".ttl_";
+        }
+
+        private async Task<bool> SetOrAdd(string key, string value, TimeSpan? expiry, bool isAdd)
+        {
+            var tasks = new Task<bool>[2];
+            tasks[0] = db.StringSetAsync(FullKey(key), value, expiry, isAdd ? When.NotExists : When.Always);
+            tasks[1] = slidingExpiry && expiry.HasValue
+                ? db.StringSetAsync(ExpiryKey(key), (long)expiry.Value.TotalMilliseconds, expiry, isAdd ? When.NotExists : When.Always)
+                : Task.FromResult(true);
+            await Task.WhenAll(tasks);
+            if (tasks[0].Result)
+            {
+                PublishSet(key);
+            }
+            return tasks[0].Result;
+        }
+
+        public Task<bool> Add(string key, string value, TimeSpan? expiry)
+        {
+            if (key == null)
+            {
+                throw new ArgumentException("Cache key cannot be null.", nameof(key));
+            }
+            if (value == null)
+            {
+                throw new ArgumentException("Cache value cannot be null.", nameof(value));
+            }
+            return SetOrAdd(key, value, expiry, true);
+        }
+
+        public Task Set(string key, string value, TimeSpan? expiry)
+        {
+            if (key == null)
+            {
+                throw new ArgumentException("Cache key cannot be null.", nameof(key));
+            }
+            if (value == null)
+            {
+                throw new ArgumentException("Cache value cannot be null.", nameof(value));
+            }
+            return SetOrAdd(key, value, expiry, false);
+        }
+
+        class CacheValue : ICacheValue<string>
+        {
+            public bool HasValue { get; set; }
+            public string Value { get; set; }
+        }
+
+        public async Task<ICacheValue<string>> Get(string key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentException("Cache key cannot be null.", nameof(key));
+            }
+            var tasks = new Task<RedisValue>[2];
+            tasks[0] = db.StringGetAsync(FullKey(key));
+            tasks[1] = slidingExpiry ? db.StringGetAsync(ExpiryKey(key)) : Task.FromResult(RedisValue.Null);
+            var results = await Task.WhenAll(tasks);
+            TimeSpan? expiry = null;
+            if (results[0].HasValue && results[1].HasValue)
+            {
+                expiry = TimeSpan.FromMilliseconds((long)results[1]);
+            }
+            if (results[0].HasValue && expiry.HasValue)
+            {
+                var nowait = db.KeyExpireAsync(FullKey(key), expiry);
+                nowait = db.KeyExpireAsync(ExpiryKey(key), expiry);
+            }
+            var value = new CacheValue();
+            if (results[0].HasValue)
+            {
+                value.HasValue = true;
+                value.Value = results[0];
+            }
+            return value;
+        }
+
+        public async Task<bool> Delete(string key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentException("Cache key cannot be null.", nameof(key));
+            }
+            var tasks = new Task<bool>[2];
+            tasks[0] = db.KeyDeleteAsync(FullKey(key));
+            tasks[1] = slidingExpiry ? db.KeyDeleteAsync(ExpiryKey(key)) : Task.FromResult(true);
+            await Task.WhenAll(tasks);
+            if (tasks[0].Result)
+            {
+                PublishRemove(key);
+            }
+            return tasks[0].Result;
+        }
+
+        private void PublishSet(string key)
+        {
+            if (sub != null)
+            {
+                var nowait = sub.PublishAsync(keySetChannel, key);
+            }
+        }
+
+        private void PublishRemove(string key)
+        {
+            if (sub != null)
+            {
+                var nowait = sub.PublishAsync(keyDelChannel, key);
+            }
+        }
+
+        public Task<Guid> Subscribe(string key, Action<string> onSet, Action<string> onDelete)
+        {
+            return Subscribe(key, async k => { onSet(k); await Task.FromResult(true); }, async k => { onDelete(k); await Task.FromResult(true); });
+        }
+
+        public async Task<Guid> Subscribe(string key, Func<string, Task> onSet, Func<string, Task> onDelete)
+        {
+            if (key == null)
+            {
+                throw new ArgumentException("Cache key cannot be null.", nameof(key));
+            }
+            if (sub == null)
+            {
+                await subSync.WaitAsync();
+                try
+                {
+                    // Check again once we have the lock.
+                    if (sub == null)
+                    {
+                        sub = connection.GetSubscriber();
+                        await sub.SubscribeAsync(keySetChannel, keySetHandler);
+                        await sub.SubscribeAsync(keyDelChannel, keyDelHandler);
+                    }
+                }
+                finally
+                {
+                    subSync.Release();
+                }
+            }
+            return subHelper.AddSubscription(key, onSet, onDelete);
+        }
+
+        public Task Unsubscribe(string key, Guid subscriptionId)
+        {
+            if (key == null)
+            {
+                throw new ArgumentException("Cache key cannot be null.", nameof(key));
+            }
+            subHelper.RemoveSubscription(key, subscriptionId);
+            return Task.FromResult(true);
+        }
+
+        public void Dispose()
+        {
+            if (sub != null)
+            {
+                sub.Unsubscribe(keySetChannel, keySetHandler);
+                sub.Unsubscribe(keyDelChannel, keyDelHandler);
+            }
+        }
+    }
+}
---
+++ src/MindSung.Caching.Providers.Redis/RedisProviderFactory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using StackExchange.Redis;
+
+namespace MindSung.Caching.Providers.Redis
+{
+    public class RedisProviderFactory : ICacheProviderFactory, IDisposable
+    {
+        public RedisProviderFactory(string networkAddress, string password = null, bool ssl = false, int port = 0)
+        {
+            this.networkAddress = networkAddress;
+            this.password = password;
+            this.ssl = ssl;
+            this.port = port;
+        }
+
+        private string networkAddress;
+        private string password;
+        private bool ssl;
+        private int port;
+        private ConnectionMultiplexer connection;
+        private SemaphoreSlim connectionSync = new SemaphoreSlim(1, 1);
+
+        private async Task<ConnectionMultiplexer> GetConnection()
+        {
+            if (connection == null)
+            {
+                await connectionSync.WaitAsync();
+                try
+                {
+                    if (connection == null)
+                    {
+                        var cn = $"{networkAddress}:{(port > 0 ? port : (ssl ? 6380 : 6379))},abortConnect=false";
+                        if (ssl) { cn += ",ssl=true"; }
+                        if (password != null) { cn += $",password={password}"; }
+                        connection = await ConnectionMultiplexer.ConnectAsync(cn);
+                    }
+                }
+                finally
+                {
+                    connectionSync.Release();
+                }
+            }
+            return connection;
+        }
+
+        public async Task<ICacheProvider<T>> GetNamedCacheProvider<T>(string cacheName, bool slidingExpiry)
+        {
+            if (typeof(T) != typeof(string))
+            {
+                throw new NotSupportedException("Type parameter T for RedisProviderFactory method must be type System.String.");
+            }
+            return (ICacheProvider<T>)(new RedisProvider(await GetConnection(), cacheName, slidingExpiry));
+        }
+
+        public void Dispose()
+        {
+            if (connection != null)
+            {
+                connection.Dispose();
+            }
+        }
+    }
+}
